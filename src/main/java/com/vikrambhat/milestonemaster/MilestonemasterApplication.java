d0e302a0998df48d5c9c7c1cf419cb79f8874d4b
Vikram Bhat <89530170+BailurVikramBhat@users.noreply.github.com>
2026-05-14 19:59:38 +0530

    initial commit

---
+++ src/main/java/com/vikrambhat/milestonemaster/MilestonemasterApplication.java
@@ -0,0 +1,13 @@
+package com.vikrambhat.milestonemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestonemasterApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MilestonemasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikrambhat/milestonemaster/MilestonemasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikrambhat.milestonemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilestonemasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
